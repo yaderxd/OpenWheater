e62c497d7574696a580ddd45e8e5044bd8aa4b75
yaderxd <90113933+yaderxd@users.noreply.github.com>
2022-04-21 09:45:50 -0600

    Add files via upload

---
+++ WeatherConcurrencyApp.Infrastructure/OpenWeatherClient/HttpOpenWeatherClient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherConcurrencyApp.Common;
+using WeatherConcurrentApp.Domain.Entities;
+using WeatherConcurrentApp.Domain.Interfaces;
+
+namespace WeatherConcurrencyApp.Infrastructure.OpenWeatherClient
+{
+    public class HttpOpenWeatherClient : IHttpOpenWeatherClient
+    {
+        public async Task<OpenWeather> GetWeatherByCityNameAsync(string city)
+        {
+           
+            string url = $"{AppSettings.ApiUrl}{city}&units={AppSettings.units}&lang=sp&appid={AppSettings.Token}";
+            string jsonObject = string.Empty;
+            try
+            {
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    jsonObject = await httpClient.GetAsync(url).Result.Content.ReadAsStringAsync();
+                }
+                   
+                if(string.IsNullOrEmpty(jsonObject))
+                {
+                    throw new NullReferenceException("El objeto json no puede ser null.");
+                }
+
+                return Newtonsoft.Json.JsonConvert.DeserializeObject<OpenWeather>(jsonObject);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+           
+        }
+    }
+}
