73f20d92242da0be889e4e05eb6930b1c39d11a2
yaderxd <90113933+yaderxd@users.noreply.github.com>
2022-04-21 09:46:14 -0600

    Add files via upload

---
+++ WeatherConcurrentApp.Domain/Entities/OpenWeather.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherConcurrentApp.Domain.Entities
+{
+    public class OpenWeather
+    {
+        public Coord Coord { get; set; }
+        public List<Weather> Weather { get; set; }
+        public string Base { get; set; }
+        public Main Main { get; set; }
+        public int Visibility { get; set; }
+        public Wind Wind { get; set; }
+        public Clouds Clouds { get; set; }
+        public int Dt { get; set; }
+        public Sys Sys { get; set; }
+        public int Timezone { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Cod { get; set; }
+    }
+
+    public class Wind
+    {
+        public double Speed { get; set; }
+        public int Deg { get; set; }
+        public double Gust { get; set; }
+    }
+
+    public class Weather
+    {
+        public int Id { get; set; }
+        public string Main { get; set; }
+        public string Description { get; set; }
+        public string Icon { get; set; }
+    }
+
+    public class Sys
+    {
+        public string Country { get; set; }
+        public int Sunrise { get; set; }
+        public int Sunset { get; set; }
+    }
+
+    public class Main
+    {
+        public double Temp { get; set; }
+        public double Feels_like { get; set; }
+        public double Temp_min { get; set; }
+        public double Temp_max { get; set; }
+        public int Pressure { get; set; }
+        public int Humidity { get; set; }
+        public int Sea_level { get; set; }
+        public int Grnd_level { get; set; }
+    }
+
+    public class Coord
+    {
+        public double Lon { get; set; }
+        public double Lat { get; set; }
+    }
+
+    public class Clouds
+    {
+        public int All { get; set; }
+    }
+}
---
+++ WeatherConcurrentApp.Domain/Interfaces/IHttpOpenWeatherClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherConcurrentApp.Domain.Entities;
+
+namespace WeatherConcurrentApp.Domain.Interfaces
+{
+    public interface IHttpOpenWeatherClient
+    {
+       Task<OpenWeather> GetWeatherByCityNameAsync(string city);
+    }
+}
